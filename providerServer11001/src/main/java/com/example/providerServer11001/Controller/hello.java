2aa52b28dd0c7fa26d14845114d2e478b7cb910a
Karlare <105634593+Karlare@users.noreply.github.com>
2023-05-18 20:16:10 +0800

    软件服务工程实验

--- providerServer11001/src/main/java/com/example/providerServer11001/Controller/hello.java
+++ providerServer11001/src/main/java/com/example/providerServer11001/Controller/hello.java
@@ -1,14 +1,21 @@
 package com.example.providerServer11001.Controller;
-
+import org.springframework.beans.factory.annotation.Value;
 import com.example.providerServer11001.Entity.CommonResult;
 import com.example.providerServer11001.Entity.User;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@RefreshScope //开启动态刷新
 public class hello {
+
+    //获取配置信息中的name属性值
+    @Value("${spring.application.msg}")
+    private String msg;
+
     @RequestMapping("/hello")
     public String hello(){
         return "hello!!!";
@@ -16,6 +23,6 @@ public class hello {
 
     @RequestMapping("/getUserById/{userID}")
     public CommonResult<User> getuserById(@PathVariable("userID") Integer userID){
-        return new CommonResult(200,"success(11001)",new User(userID,"李四","12345"));
+        return new CommonResult(200,"success(11001)(msg:"+msg+")",new User(userID,"李四","67890"));
     }
 }
